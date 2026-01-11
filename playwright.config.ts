32b8393c9dc2bb9af1e5e4b5c0f62471c8ba5ba0
EirikViking <152316087+EirikViking@users.noreply.github.com>
2026-01-11 11:04:20 +0100

    test: add MVP e2e coverage

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
     reporter: 'html',
     use: {
         baseURL: 'http://localhost:5199',
-        trace: 'on-first-retry',
-        screenshot: 'only-on-failure',
+        trace: 'off',
+        screenshot: 'off',
     },
     projects: [
         {
